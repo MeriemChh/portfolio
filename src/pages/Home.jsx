74c3783b41710b0ecae29ff24028b14c5676037c
Meriem Chakhoum <meriemchakhoum@gmail.com>
2025-07-02 20:20:33 +0100

    Fixed typo in HeroSection import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import Herosection from '../components/Herosection';
+import Herosection from '../components/HeroSection';
 import ProjectsSection from '../components/ProjectsSection';
 import '../styles/Home.css';
 import Footer from '../components/Footer';
